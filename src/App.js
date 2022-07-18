cdaef22fb7f28b354cf9b45cb25b4911781251dd
LazarevEV <lazarev.evgeniy.nf@gmail.com>
2022-07-18 20:03:50 +0300

    init repo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import { React, useState } from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import About from './page/About'
-import NotFound from './page/NotFound'
-import OrderTotals from './page/OrderTotals'
-import LoginPass from './components/LoginPass'
-import Order from './components/Order'
-import BookDesc from './components/BookDesc'
-import UserOrder from './components/UserOrder'
-import OrderReady from './components/OrderReady'
 import UserInfoContext from './context/UserInfoContext'
-import OrderInfoContext from './context/OrderInfoContext'
 
 function App() {
 	const [user, setUser] = useState({
@@ -23,19 +14,11 @@ function App() {
 
 	return (
 		<UserInfoContext.Provider value={{user, setUser}}>
-			<OrderInfoContext.Provider value={{order, setOrder}}>
-				<div className="App">
-					<Routes>
-						<Route path="/" element={<LoginPass />} />
-						<Route path="/order" element={<Order />}/>
-						<Route path="/book" element={<BookDesc />}/>
-						<Route path="/user_order" element={<UserOrder />}/>
-						<Route path="/order_receipt" element={<OrderReady />}/>
-						<Route path="/about" element={<About />} />
-						<Route path="*" element={<NotFound />} />
-					</Routes>
-				</div>
-			</OrderInfoContext.Provider>
+			<div className="App">
+				<Routes>
+
+				</Routes>
+			</div>
 		</UserInfoContext.Provider>
 	)
 }
--- src/context/OrderInfoContext.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
-import React from "react";
-
-const OrderInfoContext = React.createContext({
-    order: [],
-    setOrder: () => {}
-});
-
-export default OrderInfoContext;
